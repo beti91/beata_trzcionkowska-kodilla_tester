5747d6e108434243fc6c0ca64e49b24301c4f70c
Beata Trzcionkowska <beatat91@gmail.com>
2021-12-05 20:41:54 +0100

    exer. tablice

---
+++ src/main/java/GradesAplication.java
@@ -0,0 +1,20 @@
+public class GradesAplication
+{
+    public static void main(String[] args) {
+        Grades oceny = new Grades();
+        oceny.add(5);
+        oceny.add(4);
+        oceny.add(2);
+        oceny.add(4);
+        oceny.add(5);
+        oceny.add(6);
+        oceny.add(3);
+        oceny.add(4);
+        oceny.add(5);
+        oceny.add(4);
+   oceny.getLastGrade();
+   oceny.getAverage();
+    }
+
+
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,35 @@
+public class User {
+    String imie;
+    int wiek;
+
+    public User(String imie, int wiek) {
+        this.imie = imie;
+        this.wiek = wiek;
+    }
+
+    String getImie() {
+        return this.imie;
+    }
+
+    int getWiek() {
+        return this.wiek;
+    }
+
+    public static void main(String[] args) {
+        User anna = new User("Anna", 20);
+        User betty = new User("Betty", 33);
+        User carl = new User("Carl", 58);
+        User david = new User("David", 13);
+        User eva = new User("Eva", 18);
+        User frankie = new User("Frankie", 45);
+
+        User[] users = {anna, betty, carl, david, eva, frankie};
+
+
+
+    }
+}
+
+
+
+
