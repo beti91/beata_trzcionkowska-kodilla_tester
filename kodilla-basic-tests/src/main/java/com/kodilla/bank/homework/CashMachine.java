f3268810444e42b53643a5b6aa4179674452fa44
Beata Trzcionkowska <beatat91@gmail.com>
2022-01-08 13:21:09 +0100

    exercise 3 i 4

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -0,0 +1,34 @@
+package com.kodilla.bank.homework;
+
+public class CashMachine {
+
+    private int[] values;
+    private int size;
+
+
+    public CashMachine() {
+        this.size = 0;
+        this.values = new int[0];
+
+    }
+    public void add(int value) {
+        this.size++;
+        int[] newTab = new int[this.size];
+        System.arraycopy(values, 0, newTab,0, values.length);
+        newTab[this.size-1] = value;
+        this.values = newTab;
+    }
+    public int[] getValues() {
+        return values;
+    }
+    public int saldo() {
+       int sum = 0;
+       for(int i = 0; i < values.length; i++) {
+           sum += values[i];
+       }
+        return sum;
+    }
+    public int quantity() {
+        return this.size;
+    }
+}
---
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/CashMachineTestSuite.java
@@ -0,0 +1,44 @@
+package com.kodilla.bank.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashMachineTestSuite {
+    @Test
+    public void shouldHaveZeroLength() {
+        CashMachine cashMachine = new CashMachine();
+        int[] values =cashMachine.getValues();
+        assertEquals(0, values.length);
+    }
+    @Test
+    public void shouldAddTwoElementsToArray() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(400);
+        cashMachine.add(1000);
+
+        int[] values = cashMachine.getValues();
+        assertEquals(2, values.length);
+        assertEquals(400, values[0]);
+        assertEquals(1000, values[1]);
+    }
+    @Test
+    public void testSaldo() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(500);
+        cashMachine.add(500);
+        cashMachine.add(-500);
+
+        assertEquals(500, cashMachine.saldo());
+
+
+    }
+    @Test
+    public void testOfQuantity() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(100);
+        cashMachine.add(500);
+
+        assertEquals(2, cashMachine.quantity());
+    }
+}
